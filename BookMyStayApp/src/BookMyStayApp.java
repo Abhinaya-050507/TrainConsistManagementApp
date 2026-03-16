bf338f712e72e65a5e0cf0af7125000cd8e9f16c
Abhinaya-050507 <abhinayasaraswathi557@gmail.com>
2026-03-16 14:34:38 +0530

    UC2:Basic Room Types & Static Availability

--- BookMyStayApp/src/BookMyStayApp.java
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -1,8 +1,68 @@
+abstract class Room {
+
+    protected int beds;
+    protected int size;
+    protected double pricePerNight;
+
+    public Room(int beds, int size, double pricePerNight) {
+        this.beds = beds;
+        this.size = size;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayDetails() {
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
+
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
 public class BookMyStayApp {
+
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Hotel Booking Management System");
-        System.out.println("System initialized Successfully");
+        // Create room objects
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
 
+        // Static availability variables
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Hotel Room Initialization\n");
+
+        System.out.println("Single Room:");
+        singleRoom.displayDetails();
+        System.out.println("Available: " + singleAvailable + "\n");
+
+        System.out.println("Double Room:");
+        doubleRoom.displayDetails();
+        System.out.println("Available: " + doubleAvailable + "\n");
+
+        System.out.println("Suite Room:");
+        suiteRoom.displayDetails();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
